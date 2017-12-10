ac747620df71151b05d803217d5cda96273775c5
CORP\jrichter <jan-peter.richter@capgemini.com>
2017-12-10 13:53:02 +0100

    Now the AccountingEntryDaoTest succeeds although it should fail due to @Transient Money field

--- core/src/test/java/eu/jrichter/cashplanner/bankaccountmanagement/dataaccess/api/dao/AccountingEntryDaoTest.java
+++ core/src/test/java/eu/jrichter/cashplanner/bankaccountmanagement/dataaccess/api/dao/AccountingEntryDaoTest.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 import javax.inject.Inject;
 import javax.money.Monetary;
+import javax.transaction.Transactional;
 
 import org.javamoney.moneta.Money;
 import org.junit.Test;
@@ -23,13 +24,11 @@ public class AccountingEntryDaoTest extends AbstractApplicationComponentTest {
   @Inject
   AccountingEntryDao accountingEntryDao;
 
-  @Inject
-  AccountingEntryDaoTestHelper accountingEntryDaoTestHelper;
-
   /**
    * test basic saving operations of the DAO - especially the Money field
    */
   @Test
+  @Transactional
   public void testSaveAccountingEntry() {
 
     AccountingEntryEntity entity = new AccountingEntryEntity();
@@ -39,13 +38,16 @@ public class AccountingEntryDaoTest extends AbstractApplicationComponentTest {
     entity.setPostingText("Testbuchung");
     entity.setAmount(Money.of(BigDecimal.valueOf(420815), Monetary.getCurrency("EUR")));
 
-    Long id = this.accountingEntryDaoTestHelper.saveAccountingEntry(entity);
+    AccountingEntryEntity entitySaved = this.accountingEntryDao.save(entity);
+
+    Long id = entitySaved.getId();
     assertThat(id).isNotNull();
 
     AccountingEntryEntity retrievedEntity = this.accountingEntryDao.find(id);
     assertThat(retrievedEntity.getDateOfBookkeepingEntry()).isEqualTo(entity.getDateOfBookkeepingEntry());
     assertThat(retrievedEntity.getValueDate()).isEqualTo(entity.getValueDate());
     assertThat(retrievedEntity.getPostingText()).isEqualTo(entity.getPostingText());
-    assertThat(retrievedEntity.getAmount()).isEqualTo(entity.getAmount()); // will fail due to @Transient
+    assertThat(retrievedEntity.getAmount()).isEqualTo(entity.getAmount()); // should fail due to @Transient but doesn't
+                                                                           // because of caching within Hibernate
   }
 }
