f3c7e2196d9616b7c061400cf40a574c43db9ccf
CORP\jrichter <jan-peter.richter@capgemini.com>
2017-12-17 00:09:17 +0100

    Some additions to the h2 file URL (AUTO_SERVER=TRUE) and to the dozer mapping (copy-by-reference for java.lang.LocalDate and the JSR364 Money data types) as well as a quick&dirty hack service to be able to trigger some system functions by calling a rest web service.

---
+++ core/src/main/java/eu/jrichter/cashplanner/bankaccountmanagement/service/api/rest/TestService.java
@@ -0,0 +1,25 @@
+package eu.jrichter.cashplanner.bankaccountmanagement.service.api.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import eu.jrichter.cashplanner.bankaccountmanagement.logic.api.to.AccountingEntryEto;
+
+/**
+ * The service interface for REST calls in order to execute the logic of component {@link Bankaccountmanagement}.
+ */
+@Path("/test")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
+public interface TestService {
+
+  @GET
+  @Path("/get/{id}/")
+  public AccountingEntryEto getAccountingEntry(@PathParam("id") long id);
+
+}
---
+++ core/src/main/java/eu/jrichter/cashplanner/bankaccountmanagement/service/impl/rest/TestServiceImpl.java
@@ -0,0 +1,55 @@
+package eu.jrichter.cashplanner.bankaccountmanagement.service.impl.rest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.money.Monetary;
+import javax.transaction.Transactional;
+
+import org.javamoney.moneta.Money;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.jrichter.cashplanner.bankaccountmanagement.dataaccess.api.AccountingEntryEntity;
+import eu.jrichter.cashplanner.bankaccountmanagement.dataaccess.api.dao.AccountingEntryDao;
+import eu.jrichter.cashplanner.bankaccountmanagement.logic.api.to.AccountingEntryEto;
+import eu.jrichter.cashplanner.bankaccountmanagement.service.api.rest.TestService;
+import eu.jrichter.cashplanner.general.logic.api.UseCase;
+import eu.jrichter.cashplanner.general.logic.base.AbstractUc;
+
+/**
+ * This is just a quick & dirty hack in order to be able to trigger some system behavior.
+ *
+ * @author jrichter
+ * @since 0.0.1
+ */
+@Named("TestRestService")
+@UseCase
+public class TestServiceImpl extends AbstractUc implements TestService {
+
+  @Inject
+  private AccountingEntryDao accountingEntryDao;
+
+  /** Logger instance. */
+  private static final Logger LOG = LoggerFactory.getLogger(TestServiceImpl.class);
+
+  @Override
+  @Transactional
+  public AccountingEntryEto getAccountingEntry(long id) {
+
+    LOG.info("TestService called with id " + id);
+
+    AccountingEntryEntity entity = new AccountingEntryEntity();
+    entity.setDateOfBookkeepingEntry(LocalDate.now());
+    entity.setValueDate(LocalDate.of(2017, 12, 15));
+    entity.setPostingText("Test entry");
+    entity.setAmount(Money.of(BigDecimal.valueOf(420815), Monetary.getCurrency("EUR")));
+
+    entity = this.accountingEntryDao.save(entity);
+
+    return getBeanMapper().map(entity, AccountingEntryEto.class);
+  }
+
+}
